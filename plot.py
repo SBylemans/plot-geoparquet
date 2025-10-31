172adcd4b893898b9b59862805782919235d4817
Sander Bylemans <sbylemans@gmail.com>
2025-10-31 10:18:09 +0100

    Plot parquet

--- plot.py
+++ plot.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import geopandas
 import sys
 
+print(f'Reading file: {sys.argv[1]}, Writing to: {sys.argv[2]}')
+
 df = geopandas.read_parquet(sys.argv[1])
 df.plot()
 
-plt.savefig(sys.argv[1])
+plt.savefig(sys.argv[2])
